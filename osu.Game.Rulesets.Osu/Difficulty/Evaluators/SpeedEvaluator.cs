2dc9ec4bead899d105bdd1e8b7e3d158886e3003
fridgesrunning <am_ist@outlook.com>
2024-10-20 17:03:48 -0700

    Add a nerfing multiplier for deceleration or major acceleration, targeting bursts

--- osu.Game.Rulesets.Osu/Difficulty/Evaluators/SpeedEvaluator.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Evaluators/SpeedEvaluator.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+﻿﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
 using System;
@@ -13,7 +13,7 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Evaluators
         private const double single_spacing_threshold = 125; // 1.25 circles distance between centers
         private const double min_speed_bonus = 75; // ~200BPM
         private const double speed_balancing_factor = 40;
-        private const double distance_multiplier = 0.94;
+        private const double distance_multiplier = 1;
 
         /// <summary>
         /// Evaluates the difficulty of tapping the current object, based on:
@@ -31,7 +31,46 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Evaluators
             // derive strainTime for calculation
             var osuCurrObj = (OsuDifficultyHitObject)current;
             var osuPrevObj = current.Index > 0 ? (OsuDifficultyHitObject)current.Previous(0) : null;
+            var osuL2Obj = current.Index > 1 ? (OsuDifficultyHitObject)current.Previous(1) : null;
+            var osuL3Obj = current.Index > 2 ? (OsuDifficultyHitObject)current.Previous(2) : null;
+            var osuL4Obj = current.Index > 3 ? (OsuDifficultyHitObject)current.Previous(3) : null;
+            var osuL5Obj = current.Index > 4 ? (OsuDifficultyHitObject)current.Previous(4) : null;
+            var osuL6Obj = current.Index > 5 ? (OsuDifficultyHitObject)current.Previous(5) : null;
+            var osuL7Obj = current.Index > 6 ? (OsuDifficultyHitObject)current.Previous(6) : null;
+            var osuL8Obj = current.Index > 7 ? (OsuDifficultyHitObject)current.Previous(7) : null;
 
+            // mitigate speed for anything below 9 notes by nerfing both deceleration and large acceleration, theres gotta a better way to do this but oh well
+            double deceleration =
+
+            osuCurrObj.StrainTime < 1.1 * (osuPrevObj?.StrainTime ?? 0) ? (osuPrevObj?.StrainTime ?? 0) < 1.1 * (osuL2Obj?.StrainTime ?? 0) ? (osuL2Obj?.StrainTime ?? 0) < 1.1 * (osuL3Obj?.StrainTime ?? 0) ? (osuL3Obj?.StrainTime ?? 0) < 1.1 * (osuL4Obj?.StrainTime ?? 0) ? (osuL4Obj?.StrainTime ?? 0) < 1.1 * (osuL5Obj?.StrainTime ?? 0) ? (osuL5Obj?.StrainTime ?? 0) < 1.1 * (osuL6Obj?.StrainTime ?? 0) ? (osuL6Obj?.StrainTime ?? 0) < 1.1 * (osuL7Obj?.StrainTime ?? 0) ? 
+         
+         // behavior if there has been...
+           1 :                //tapping acceleration across all checked objects
+             0.95 :           //L6 deceleration
+             0.9 :             //L5 deceleration
+              0.85 :          //L4 deceleration
+                0.8 :          //L3 deceleration
+                 0.75 :     //L2 deceleration
+                  0.7 :     //last object deceleration
+                   0.65;     //current object deceleration
+ // We also need to nerf major acceleration.
+
+            double acceleration =
+
+            osuCurrObj.StrainTime > 0.45 * (osuPrevObj?.StrainTime ?? 0) ? (osuPrevObj?.StrainTime ?? 0) > 0.45 * (osuL2Obj?.StrainTime ?? 0) ? (osuL2Obj?.StrainTime ?? 0) > 0.45 * (osuL3Obj?.StrainTime ?? 0) ? (osuL3Obj?.StrainTime ?? 0) > 0.45 * (osuL4Obj?.StrainTime ?? 0) ? (osuL4Obj?.StrainTime ?? 0) > 0.45 * (osuL5Obj?.StrainTime ?? 0) ? (osuL5Obj?.StrainTime ?? 0) > 0.45 * (osuL6Obj?.StrainTime ?? 0) ? (osuL6Obj?.StrainTime ?? 0) > 0.45 * (osuL7Obj?.StrainTime ?? 0) ?
+
+            //behavior if there is...
+            1: //no major acceleration
+            0.95:
+            0.9:
+            0.85:
+            0.8:
+            0.75:
+            0.7:
+            0.65;
+
+
+            
             double strainTime = osuCurrObj.StrainTime;
             double doubletapness = 1.0 - osuCurrObj.GetDoubletapness((OsuDifficultyHitObject?)osuCurrObj.Next(0));
 
@@ -44,7 +83,9 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Evaluators
 
             // Add additional scaling bonus for streams/bursts higher than 200bpm
             if (strainTime < min_speed_bonus)
-                speedBonus = 0.75 * Math.Pow((min_speed_bonus - strainTime) / speed_balancing_factor, 2);
+                speedBonus = 0.95 * Math.Pow((min_speed_bonus - strainTime) / speed_balancing_factor, 2);
+
+                speedBonus *= 1;
 
             double travelDistance = osuPrevObj?.TravelDistance ?? 0;
             double distance = travelDistance + osuCurrObj.MinimumJumpDistance;
@@ -53,7 +94,7 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Evaluators
             distance = Math.Min(distance, single_spacing_threshold);
 
             // Max distance bonus is 1 * `distance_multiplier` at single_spacing_threshold
-            double distanceBonus = Math.Pow(distance / single_spacing_threshold, 3.95) * distance_multiplier;
+            double distanceBonus = Math.Pow(distance / single_spacing_threshold, 4.5) * distance_multiplier;
 
             // Base difficulty with all bonuses
             double difficulty = (1 + speedBonus + distanceBonus) * 1000 / strainTime;

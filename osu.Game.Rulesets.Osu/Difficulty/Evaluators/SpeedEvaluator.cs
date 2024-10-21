2a4aebf5548ac9beda2c5695a841563a773be254
fridgesrunning <am_ist@outlook.com>
2024-10-20 17:39:05 -0700

    idk

--- osu.Game.Rulesets.Osu/Difficulty/Evaluators/SpeedEvaluator.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Evaluators/SpeedEvaluator.cs
@@ -13,7 +13,7 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Evaluators
         private const double single_spacing_threshold = 125; // 1.25 circles distance between centers
         private const double min_speed_bonus = 75; // ~200BPM
         private const double speed_balancing_factor = 40;
-        private const double distance_multiplier = 1;
+        private const double distance_multiplier = 0.94;
 
         /// <summary>
         /// Evaluates the difficulty of tapping the current object, based on:
@@ -28,7 +28,7 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Evaluators
             if (current.BaseObject is Spinner)
                 return 0;
 
-            // derive strainTime for calculation
+             // derive strainTime for calculation
             var osuCurrObj = (OsuDifficultyHitObject)current;
             var osuPrevObj = current.Index > 0 ? (OsuDifficultyHitObject)current.Previous(0) : null;
             var osuL2Obj = current.Index > 1 ? (OsuDifficultyHitObject)current.Previous(1) : null;
@@ -44,7 +44,7 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Evaluators
 
             osuCurrObj.StrainTime < 1.1 * (osuPrevObj?.StrainTime ?? 0) ? (osuPrevObj?.StrainTime ?? 0) < 1.1 * (osuL2Obj?.StrainTime ?? 0) ? (osuL2Obj?.StrainTime ?? 0) < 1.1 * (osuL3Obj?.StrainTime ?? 0) ? (osuL3Obj?.StrainTime ?? 0) < 1.1 * (osuL4Obj?.StrainTime ?? 0) ? (osuL4Obj?.StrainTime ?? 0) < 1.1 * (osuL5Obj?.StrainTime ?? 0) ? (osuL5Obj?.StrainTime ?? 0) < 1.1 * (osuL6Obj?.StrainTime ?? 0) ? (osuL6Obj?.StrainTime ?? 0) < 1.1 * (osuL7Obj?.StrainTime ?? 0) ? 
          
-         // behavior if there has been...
+       
            1 :                //tapping acceleration across all checked objects
              0.95 :           //L6 deceleration
              0.9 :             //L5 deceleration
@@ -52,8 +52,8 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Evaluators
                 0.8 :          //L3 deceleration
                  0.75 :     //L2 deceleration
                   0.7 :     //last object deceleration
-                   0.65;     //current object deceleration
- // We also need to nerf major acceleration.
+                   0.65;     //current object is deceleration
+ // We also need to do this to major acceleration (past 1/2 - 1/4)
 
             double acceleration =
 
@@ -69,8 +69,6 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Evaluators
             0.7:
             0.65;
 
-
-            
             double strainTime = osuCurrObj.StrainTime;
             double doubletapness = 1.0 - osuCurrObj.GetDoubletapness((OsuDifficultyHitObject?)osuCurrObj.Next(0));
 
@@ -83,9 +81,9 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Evaluators
 
             // Add additional scaling bonus for streams/bursts higher than 200bpm
             if (strainTime < min_speed_bonus)
-                speedBonus = 0.95 * Math.Pow((min_speed_bonus - strainTime) / speed_balancing_factor, 2);
+                speedBonus = 0.75 * Math.Pow((min_speed_bonus - strainTime) / speed_balancing_factor, 2);
 
-                speedBonus *= Math.Min(deceleration, acceleration);
+            speedBonus *= Math.Min(acceleration, deceleration);
 
             double travelDistance = osuPrevObj?.TravelDistance ?? 0;
             double distance = travelDistance + osuCurrObj.MinimumJumpDistance;
@@ -94,7 +92,7 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Evaluators
             distance = Math.Min(distance, single_spacing_threshold);
 
             // Max distance bonus is 1 * `distance_multiplier` at single_spacing_threshold
-            double distanceBonus = Math.Pow(distance / single_spacing_threshold, 4.5) * distance_multiplier;
+            double distanceBonus = Math.Pow(distance / single_spacing_threshold, 3.95) * distance_multiplier;
 
             // Base difficulty with all bonuses
             double difficulty = (1 + speedBonus + distanceBonus) * 1000 / strainTime;
